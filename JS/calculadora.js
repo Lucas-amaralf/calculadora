3e7a1c2eaf42058c88bc76130e50b4f32f8a7b02
Lucas Amaral Alves <lucas.amaralf@outlook.com>
2021-08-11 20:35:37 -0300

    calculadora

---
+++ JS/calculadora.js
@@ -0,0 +1,22 @@
+function insert(num) {
+    var numero = document.querySelector('.resultado').innerHTML;
+    document.querySelector('.resultado').innerHTML = numero + num
+}
+
+function clean() {
+    document.querySelector('.resultado').innerHTML = "";
+    document.querySelector('.resultado1').innerHTML = "";
+}
+
+function back() {
+    var resultado = document.querySelector('.resultado').innerHTML;
+    document.querySelector('.resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+function calcular () {
+    var resultado = document.querySelector('.resultado').innerHTML;
+    if (resultado) {
+        document.querySelector('.resultado1').innerHTML = eval(resultado); 
+    }
+
+    else {}
+}
